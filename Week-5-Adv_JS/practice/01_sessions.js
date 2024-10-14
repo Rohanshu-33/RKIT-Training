4f0293415dea17c44e18422f0ffbee7cb50e8144
Rohanshu-33 <banodharohanshu@gmail.com>
2024-10-14 11:21:41 +0530

    adv js started

---
+++ Week-5-Adv_JS/practice/01_sessions.js
@@ -0,0 +1,88 @@
+function validateForm() {
+    const fullname = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const pswd = document.getElementById("pswd").value;
+    const address = document.getElementById("address").value;
+    const resume = document.getElementById("resume").value;
+    const male = document.getElementById("male").checked;
+    const female = document.getElementById("female").checked;
+    let gender = null
+
+    const namePattern = /^[a-zA-Z\s]+$/;
+    const emailPattern = /^[a-zA-Z0-9._]+@[a-zA-Z]+\.[a-zA-Z]+$/;
+    const passwordPattern = /^[^\s]{8,20}$/;
+
+    if (!namePattern.test(fullname)) {
+        alert("Enter a valid name (letters and spaces only).");
+        return false;
+    }
+    sessionStorage.setItem("fullname",fullname);
+
+
+    if (!emailPattern.test(email)) {
+        alert("Enter a valid email.");
+        return false;
+    }
+    sessionStorage.setItem("email", email);
+    
+    if (!passwordPattern.test(pswd)) {
+        alert("Password must be 8-20 characters long with no spaces.");
+        return false;
+    }
+    
+    if (address.trim() === "") {
+        alert("Address is required.");
+        return false;
+    }
+    sessionStorage.setItem("address", address);
+
+    if (resume === "") {
+        alert("Please upload your resume.");
+        return false;
+    }
+
+    if (!male && !female) {
+        alert("Please select your gender.");
+        return false;
+    }
+    gender = male ?  "male" : "female"
+    sessionStorage.setItem("gender", gender);
+
+    return true;
+}
+
+
+console.log("fullname : ", sessionStorage.getItem("fullname"));
+console.log("email : ", sessionStorage.getItem("email"));
+console.log("address : ", sessionStorage.getItem("address"));
+console.log("gender : ", sessionStorage.getItem("gender"));
+
+
+if(sessionStorage.getItem("fullname")){
+    document.querySelector("#name").value = sessionStorage.getItem("fullname");
+}
+
+if(sessionStorage.getItem("address")){
+    document.querySelector("#address").value = sessionStorage.getItem("address");
+}
+
+if(sessionStorage.getItem("email")){
+    document.querySelector("#email").value = sessionStorage.getItem("email");
+}
+
+if(sessionStorage.getItem("gender")){
+    document.querySelector(`#${sessionStorage.getItem("gender")}`).checked = true;
+}
+
+
+function clearData(){
+    sessionStorage.clear();
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("pswd").value = "";
+    document.getElementById("address").value = "";
+    document.getElementById("resume").value = "";
+    document.getElementById("male").checked = false;
+    document.getElementById("female").checked = false;
+    return true
+}
---
+++ Week-5-Adv_JS/practice/02_locals.js
@@ -0,0 +1,51 @@
+
+// check whether login credentials are in localstorage or not and accordingly navigate the user
+
+function handleLogin(){
+    const email = document.getElementById("email").value;
+    const pswd = document.getElementById("password").value;
+
+    const emailPattern = /^[a-zA-Z0-9._]+@[a-zA-Z]+\.[a-zA-Z]+$/;
+    const passwordPattern = /^[^\s]{8,20}$/;
+
+    if (!emailPattern.test(email)) {
+        alert("Enter a valid email.");
+        return false;
+    }
+
+    if (!passwordPattern.test(pswd)) {
+        alert("Password must be 8-20 characters long with no spaces.");
+        return false;
+    }
+
+    localStorage.setItem("email", email);
+    localStorage.setItem("password", pswd);
+    
+    return true;
+}
+
+
+function checkAuths(){
+    const email = localStorage.getItem('email');
+    const pswd = localStorage.getItem('password');
+    
+    if(!(email && pswd)){
+        window.location.href = "/practice/login.html";
+    }
+    return true;
+}
+
+function checkAuths2(){
+    const email = localStorage.getItem('email');
+    const pswd = localStorage.getItem('password');
+    
+    if(email && pswd){
+        window.location.href = "/practice/index.html";
+    }
+    return true;
+}
+
+function handleLogout(){
+    localStorage.clear()
+    window.location.href = "/practice/login.html";
+}
